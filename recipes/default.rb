d4803b4296877a9722b9861977058faafe933242
shambhu <shambsoulmate@hotmail.com>
2018-10-25 19:56:01 -0500

    created a three files for rhel6 and rehl7 to creat a user

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,14 @@
 # Cookbook:: base
 # Recipe:: default
 #
-# Copyright:: 2018, The Authors, All Rights Reserved.
+# Copyright:: 2018, The Authors, All Rights Reserved.c
+
+case node[platform] 
+when "redhat"
+    if node[:platform_version].to_f >= 7.0
+        include_recipe "base::rhel7"
+    else 
+        include_recipe "base::rhel6"
+    end
+end
+end
---
+++ recipes/rhel6.rb
@@ -0,0 +1,11 @@
+user 'satya' do
+  comment 'Satya Saranu'
+  uid '4000'
+  gid '5000'
+  home '/home/satya'
+  shell 'shell'
+  password '$6$cIlrYUyztGlV/24E$EQ/UW02fxlMzTvjvNuZu8VC53IVCeGl5OJk..LVQ7Li6WEWxezhxmHt
+  FW3aHSFUZ4XIApTaxEF/J06uhxiXXm0::0:99999:7:::'
+  action :create
+end
+
